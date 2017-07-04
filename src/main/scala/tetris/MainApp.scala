822869e579df6fe347eecd524703d0f84bbd3d61
visernchua <elward_chua@hotmail.com>
2017-07-05 00:38:13 +0800

    Make some changes on files

--- src/main/scala/tetris/MainApp.scala
+++ src/main/scala/tetris/MainApp.scala
@@ -12,19 +12,23 @@ object MainApp extends JFXApp {
 
 
     // transform path of RootLayout.fxml to InputStream for resource location.
+    // val rootResource = getClass.getResourceAsStream("view/MainView.fxml")
     val rootResource = getClass.getResourceAsStream("view/Board.fxml")
     // initialize the loader object.
     val loader = new FXMLLoader(null, NoDependencyResolver)
     // Load root layout from fxml file.
     loader.load(rootResource);
     // retrieve the root component BorderPane from the FXML 
+    // val roots = loader.getRoot[jfxs.layout.BorderPane]
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
 
     stage = new PrimaryStage {
-        title = "AddressApp"
+        title = "Tetris"
         scene = new Scene {
           root = roots
         }
     }
     stage.setResizable(false)
+
+
 }
